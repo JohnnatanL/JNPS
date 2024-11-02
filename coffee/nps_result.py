d84ea836bb6dddb9cd2975309c91e8019490fdb7
JohnnatanL <johnnatan.cavalcante@outlook.com>
2024-11-02 17:39:26 -0300

    JNPS-v0.02112024

--- coffee/nps_result.py
+++ coffee/nps_result.py
@@ -1,3 +1,20 @@
 import streamlit as st
 
-st.header("Resultados do NPS")
+# Define a senha correta (idealmente, use uma variável de ambiente)
+PASSWORD = "cafe.10.27"
+
+# Função de autenticação
+def check_password():
+    st.title("Login")
+    password = st.text_input("Senha:", type="password")
+    if password == PASSWORD:
+        return True
+    else:
+        st.error("Senha incorreta")
+        return False
+
+# Verifica a senha antes de mostrar o conteúdo
+if check_password():
+    st.header("Resultados do NPS")
+    # Coloque o conteúdo protegido aqui
+    st.write("Aqui estão os resultados do NPS.")
